1c28a22e8cb74bb73003ea9c2512b09ac2f00732
Umar Bisharat <muhammadumarjatt303@gmail.com>
2026-02-20 15:36:59 +0500

    Update main.dart

--- 05.Chapter 5/01-part 1/main.dart
+++ 05.Chapter 5/01-part 1/main.dart
@@ -1,3 +1,7 @@
+// in the main.dart,you dont need to import every screen, only the first screen will be written after home : _______
+// Main.dart is always stateless widget
+//we usually use one scaffold per screen and we use MaterialApp for every screen
+
 import 'package:flutter/material.dart';
 import 'package:untitled1/home_screen.dart'; //The import in main.dart is just connecting main.dart with HomeScreen so Flutter knows what widget to show first.
 
