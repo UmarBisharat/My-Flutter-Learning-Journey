4487c915b26bceecf9e452a04ad61e60c1b387f4
Umar Bisharat <muhammadumarjatt303@gmail.com>
2026-02-21 13:11:00 +0500

    Initialize main.dart for Flutter application

---
+++ 05.Chapter 5/03-part 2/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/counter_app.dart';
+import 'package:untitled1/home_screen.dart';
+import 'package:untitled1/lottery_app.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id : (context) => HomeScreen(),
+        LotteryApp.id : (context) => LotteryApp(),
+        CounterApp.id : (context) => CounterApp(),
+      },
+    );
+  }
+}
+
+
+
+
