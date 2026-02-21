b71f19393badaa668829410c334f6c94a3a3d31a
Umar Bisharat <muhammadumarjatt303@gmail.com>
2026-02-21 13:12:22 +0500

    \

---
+++ 05.Chapter 5/03-part 2/home_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/lottery_app.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String id = 'home_screen';
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'The First Screen',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 22),
+          Text(
+            'Log In',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 22),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'user name',
+                prefixIcon: Icon(Icons.tag_faces_sharp),
+                fillColor: Colors.redAccent.withOpacity(.5),
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(55),
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(55),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 22),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                hintText: 'password',
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: Icon(Icons.remove_red_eye),
+                fillColor: Colors.redAccent.withOpacity(.5),
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(55),
+                  borderSide: BorderSide(color: Colors.white),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(55),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 240),
+            child: Text('forget password'),
+          ),
+          SizedBox(height: 55),
+          Container(
+            height: 50,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.redAccent.withOpacity(.8),
+              borderRadius: BorderRadius.circular(55),
+              border: Border.all(color: Colors.white70, width: 3),
+              boxShadow: [BoxShadow(color: Colors.red, blurRadius: 30)],
+            ),
+            child: TextButton(
+              onPressed: () {
+               Navigator.pushNamed(context, LotteryApp.id);
+              },
+              child: Text(
+                'Log In',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          RichText(
+            text: TextSpan(
+              text: "Don't have an account ?",
+              style: TextStyle(color: Colors.black),
+              children: [
+                TextSpan(
+                  text: "  Sign Up",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
