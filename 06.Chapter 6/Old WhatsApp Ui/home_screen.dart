68f25e49aaf9c47812ed20bd79ad1d807520576f
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-03-11 16:25:38 +0500

    Update home_screen.dart

--- 06.Chapter 6/Old WhatsApp Ui/home_screen.dart
+++ 06.Chapter 6/Old WhatsApp Ui/home_screen.dart
@@ -12,25 +12,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: 4, // number of tabs
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.teal,
           title: Text('WhatsApp', style: TextStyle(color: Colors.white)),
+
+          // Tabs start here
           bottom: TabBar(
             tabs: [
-              Tab(child: Icon(Icons.camera_alt, color: Colors.white)),
-              Tab(
-                child: Text('Chats', style: TextStyle(color: Colors.white)),
-              ),
-              Tab(
-                child: Text('Status', style: TextStyle(color: Colors.white)),
-              ),
-              Tab(
-                child: Text('Calls', style: TextStyle(color: Colors.white)),
-              ),
+              Tab(icon: Icon(Icons.camera_alt, color: Colors.white)),
+              Tab(child: Text('Chats', style: TextStyle(color: Colors.white))),
+              Tab(child: Text('Status', style: TextStyle(color: Colors.white))),
+              Tab(child: Text('Calls', style: TextStyle(color: Colors.white))),
             ],
           ),
+
+          // AppBar actions
           actions: [
             Icon(Icons.search, color: Colors.white),
             SizedBox(width: 10),
@@ -45,15 +43,16 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(width: 10),
           ],
         ),
+
         body: TabBarView(
           children: [
 
-            // Camera
-            
+            // ================= CAMERA TAB START =================
             Text('Camera'),
+            // ================= CAMERA TAB END =================
 
-            // Chats
-            
+
+            // ================= CHATS TAB START =================
             ListView.builder(
               itemCount: 10,
               itemBuilder: (context, index) {
@@ -69,9 +68,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 );
               },
             ),
+            // ================= CHATS TAB END =================
 
-            // Status
-            
+
+            // ================= STATUS TAB START =================
             ListView.builder(
               itemCount: 10,
               itemBuilder: (context, index) {
@@ -79,10 +79,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   return Padding(
                     padding: const EdgeInsets.symmetric(vertical: 10),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(' New updates'),
+                        Text('New updates'),
                         ListTile(
                           leading: Container(
                             decoration: BoxDecoration(
@@ -103,52 +102,32 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   );
                 } else {
-                  Padding(
+                  return Padding(
                     padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('new updates'),
-                        ListTile(
-                          leading: Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.green, width: 5),
-                            ),
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                'https://media.licdn.com/dms/image/v2/D4D03AQFSfWlsXowPug/profile-displayphoto-scale_400_400/B4DZv4HQzKJoAg-/0/1769394214008?e=1773878400&v=beta&t=7Ql5nN-e2jqOPIWA5BUi1B87Ibt9mLvBSvQLcdbL4Gk',
-                              ),
-                            ),
-                          ),
-                          title: Text('Umar Bisharat'),
-                          subtitle: Text('Alhamdulillah'),
-                          trailing: Text('3:09 pm'),
+                    child: ListTile(
+                      leading: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.green, width: 5),
                         ),
-                      ],
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            'https://media.licdn.com/dms/image/v2/D4D03AQFSfWlsXowPug/profile-displayphoto-scale_400_400/B4DZv4HQzKJoAg-/0/1769394214008?e=1773878400&v=beta&t=7Ql5nN-e2jqOPIWA5BUi1B87Ibt9mLvBSvQLcdbL4Gk',
+                          ),
+                        ),
+                      ),
+                      title: Text('Umar Bisharat'),
+                      subtitle: Text('Alhamdulillah'),
+                      trailing: Text('3:09 pm'),
                     ),
                   );
                 }
-
-                return ListTile(
-                  leading: Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.green, width: 5),
-                    ),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        'https://media.licdn.com/dms/image/v2/D4D03AQFSfWlsXowPug/profile-displayphoto-scale_400_400/B4DZv4HQzKJoAg-/0/1769394214008?e=1773878400&v=beta&t=7Ql5nN-e2jqOPIWA5BUi1B87Ibt9mLvBSvQLcdbL4Gk',
-                      ),
-                    ),
-                  ),
-                  title: Text('Umar Bisharat'),
-                  subtitle: Text('Alhamdulillah'),
-                  trailing: Text('3:09 pm'),
-                );
               },
             ),
+            // ================= STATUS TAB END =================
+
+
+            // ================= CALLS TAB START =================
             ListView.builder(
               itemCount: 10,
               itemBuilder: (context, index) {
@@ -160,17 +139,18 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   title: Text('Umar Bisharat'),
                   subtitle: Text(
-                    index / 2 == 0
-                        ? 'you missed audio call'
-                        : 'call time is 3:27 pm',
+                    index % 2 == 0
+                        ? 'You missed audio call'
+                        : 'Call time is 3:27 pm',
                   ),
                   trailing: Icon(
-                    index / 2 == 0 ? Icons.call_end : Icons.video_call,
+                    index % 2 == 0 ? Icons.call_end : Icons.video_call,
                     color: Colors.red,
                   ),
                 );
               },
             ),
+            // ================= CALLS TAB END =================
           ],
         ),
       ),
