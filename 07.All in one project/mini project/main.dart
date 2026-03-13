5e3d5c19a96ada1d8e31529002a0a22ee69b2f85
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-03-13 14:55:09 +0500

    Initialize Flutter app with routing setup

---
+++ 07.All in one project/mini project/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/about.dart';
+import 'package:untitled/screen_1.dart';
+import 'package:untitled/screen_2.dart';
+import 'package:untitled/screen_3.dart';
+import 'package:untitled/screen_4.dart';
+import 'package:untitled/screen_5.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Screen1.id,
+      routes: {
+        Screen1.id : (context) => Screen1(),
+        Screen2.id : (context) => Screen2(),
+        Screen3.id : (context) => Screen3(),
+        Screen4.id : (context) => Screen4(),
+        Screen5.id : (context) => Screen5(),
+        About.id : (context) => About(),
+      },
+    );
+  }
+}
