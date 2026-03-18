cb7272a6acaf5b1280dd2d6ffb7b3840ea521296
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-03-19 00:12:45 +0500

    Create contant.dart

---
+++ 10.Chapter 8/02.part 2/contant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color myChosenColor = Colors.white;
+
+const Color mySelectedColor = Colors.pink;
+
+const Color myColor = Colors.white;
+
+const myTextStyle = TextStyle(
+  color: myChosenColor,
+);
