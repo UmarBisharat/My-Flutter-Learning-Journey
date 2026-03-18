de3ba2a06635ac50a1644abc2a4f6c4095722d21
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-03-18 18:12:37 +0500

    Create constant.dart

---
+++ 10.Chapter 8/01.part 1/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color myChosenColor = Colors.purple;
+
+const Color mySelectedColor = Colors.pink;
+
+const myTextStyle = TextStyle(
+  fontSize: 40,
+  color: myChosenColor,
+  fontWeight: FontWeight.bold,
+);
