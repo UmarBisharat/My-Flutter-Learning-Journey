42211b2de657d9d15bb7737798af95e8c0144a9f
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-05-23 12:05:28 +0500

    Create splash_screen.dart

---
+++ 12.Chapter 9/01.part 1/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:untitled/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('object');
+    return Scaffold(
+      body: Image(
+        height: double.infinity,
+        fit: BoxFit.fitHeight,
+        image: NetworkImage(
+          'https://images.pexels.com/photos/32140540/pexels-photo-32140540/free-photo-of-vintage-camera-on-white-fabric-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+        ),
+      ),
+    );
+  }
+}
