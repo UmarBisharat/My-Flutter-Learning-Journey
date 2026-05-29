fd36678210a57f92547c1ecc73878189a3ea5e31
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-05-29 11:25:46 +0500

    Update main.dart

--- 12.Chapter 9/01.part 1/main.dart
+++ 12.Chapter 9/01.part 1/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/splash_screen.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+home: SplashScreen(),
+    );
   }
 }
