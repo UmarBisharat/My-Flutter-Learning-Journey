0e3b199f004bf76282444ec44b049e54075bbf39
Umar Bisharat <192307107+UmarBisharat@users.noreply.github.com>
2026-05-29 11:26:51 +0500

    Update home_screen.dart

--- 12.Chapter 9/01.part 1/home_screen.dart
+++ 12.Chapter 9/01.part 1/home_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+backgroundColor: Colors.green,
+    );
   }
 }
